ca3eb586b65a21e779243863d62ecb038bec25bc
Loic-Chasseray <109997166+Loic-Chasseray@users.noreply.github.com>
2026-06-04 11:57:00 +0100

    Update collect.js

--- collect.js
+++ collect.js
@@ -31,8 +31,8 @@ async function main()
 {
     const newRows = await fetchData()
     const existingRows = loadMaster()
-    console.log("New rows:, newRows.length")
-    console.log("Existing rows:, existingRows.length")
+    console.log("New rows:", newRows.length)
+    console.log("Existing rows:", existingRows.length)
 }
 
 main()
